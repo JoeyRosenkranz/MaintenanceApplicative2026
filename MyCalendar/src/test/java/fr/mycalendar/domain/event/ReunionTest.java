efea738e57ab82d2d71ceed1c7b93c5da4a9a37c
JoeyRosenkranz <joeykaizoku@gmail.com>
2026-03-23 09:21:31 +0100

    ajout du test de la réunion

---
+++ MyCalendar/src/test/java/fr/mycalendar/domain/event/ReunionTest.java
@@ -0,0 +1,26 @@
+package fr.mycalendar.domain.event;
+
+import fr.mycalendar.domain.vo.*;
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReunionTest {
+    @Test
+    void instancieReunion() {
+        Evenement reunion = new Reunion(
+                EventId.nouveau(),
+                new TitreEvenement("Point hebdo"),
+                new DateEvenement(LocalDate.now()),
+                new HeureDebut(LocalTime.now()),
+                new DureeEvenement(Duration.ofMinutes(60)),
+                new DescriptionEvenement("Status"),
+                new Lieu("Salle A"),
+                new Participants(List.of(new Participant("Alice")))
+        );
+        assertNotNull(reunion);
+    }
+}
