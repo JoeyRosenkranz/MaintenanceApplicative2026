a27734e724d695cc9f7da5fc2daba7a26a1826f4
JoeyRosenkranz <joeykaizoku@gmail.com>
2026-03-23 09:18:56 +0100

    implémentation minimale de la description

---
+++ MyCalendar/src/main/java/fr/mycalendar/domain/vo/DescriptionEvenement.java
@@ -0,0 +1,13 @@
+package fr.mycalendar.domain.vo;
+
+public final class DescriptionEvenement {
+    private final String valeur;
+
+    public DescriptionEvenement(String valeur) {
+        this.valeur = valeur;
+    }
+
+    public String valeur() {
+        return valeur;
+    }
+}
