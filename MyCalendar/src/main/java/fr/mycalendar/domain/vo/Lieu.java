ffefb9fbe0f410c132ec3cb4c0a6f64e3d02ba25
JoeyRosenkranz <joeykaizoku@gmail.com>
2026-03-23 09:19:42 +0100

    implémentation minimale du lieu

---
+++ MyCalendar/src/main/java/fr/mycalendar/domain/vo/Lieu.java
@@ -0,0 +1,13 @@
+package fr.mycalendar.domain.vo;
+
+public final class Lieu {
+    private final String valeur;
+
+    public Lieu(String valeur) {
+        this.valeur = valeur;
+    }
+
+    public String valeur() {
+        return valeur;
+    }
+}
