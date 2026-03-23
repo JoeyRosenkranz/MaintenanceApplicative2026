ffcb568feda04a968a1b6239383c30070b3e6cf2
JoeyRosenkranz <joeykaizoku@gmail.com>
2026-03-23 09:24:01 +0100

    ajout du test de présence dans la période pour rendez-vous personnel

--- MyCalendar/src/test/java/fr/mycalendar/domain/event/RendezVousPersonnelTest.java
+++ MyCalendar/src/test/java/fr/mycalendar/domain/event/RendezVousPersonnelTest.java
@@ -20,4 +20,18 @@ class RendezVousPersonnelTest {
         );
         assertNotNull(rdv);
     }
+
+    @Test
+    void estDansPeriode() {
+        Evenement rdv = new RendezVousPersonnel(
+                EventId.nouveau(),
+                new TitreEvenement("RDV Docteur"),
+                new DateEvenement(LocalDate.of(2023, 1, 1)),
+                new HeureDebut(LocalTime.of(10, 0)),
+                new DureeEvenement(Duration.ofMinutes(30)),
+                new DescriptionEvenement("Routine")
+        );
+        Periode p = new Periode(LocalDateTime.of(2023, 1, 1, 9, 0), LocalDateTime.of(2023, 1, 1, 11, 0));
+        assertTrue(((RendezVousPersonnel) rdv).estDansPeriode(p));
+    }
 }
