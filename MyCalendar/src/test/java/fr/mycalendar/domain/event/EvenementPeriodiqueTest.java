98151e72e686c7812f18a764fc3d31fce12fa39e
JoeyRosenkranz <joeykaizoku@gmail.com>
2026-03-23 09:22:19 +0100

    ajout du test de l'événement périodique

---
+++ MyCalendar/src/test/java/fr/mycalendar/domain/event/EvenementPeriodiqueTest.java
@@ -0,0 +1,25 @@
+package fr.mycalendar.domain.event;
+
+import fr.mycalendar.domain.vo.*;
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EvenementPeriodiqueTest {
+    @Test
+    void instancieEvenementPeriodique() {
+        Evenement base = new RendezVousPersonnel(
+                EventId.nouveau(),
+                new TitreEvenement("Sport"),
+                new DateEvenement(LocalDate.now()),
+                new HeureDebut(LocalTime.now()),
+                new DureeEvenement(Duration.ofMinutes(60)),
+                new DescriptionEvenement("Gym")
+        );
+        Evenement periodique = new EvenementPeriodique(base, FrequenceRepetition.HEBDOMADAIRE);
+        assertNotNull(periodique);
+        assertEquals(base.id(), periodique.id());
+    }
+}
