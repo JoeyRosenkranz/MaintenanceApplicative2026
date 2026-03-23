660c70cd0537e7bca71eb91c8bda60b0838f0773
JoeyRosenkranz <joeykaizoku@gmail.com>
2026-03-23 09:16:54 +0100

    implémentation minimale du titre

---
+++ MyCalendar/src/main/java/fr/mycalendar/domain/vo/TitreEvenement.java
@@ -0,0 +1,13 @@
+package fr.mycalendar.domain.vo;
+
+public final class TitreEvenement {
+    private final String valeur;
+
+    public TitreEvenement(String valeur) {
+        this.valeur = valeur;
+    }
+
+    public String valeur() {
+        return valeur;
+    }
+}
