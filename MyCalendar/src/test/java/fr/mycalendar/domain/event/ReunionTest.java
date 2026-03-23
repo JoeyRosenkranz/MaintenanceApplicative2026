5e7740d8345e57cd5b86c0f558bfb3a90589c686
JoeyRosenkranz <joeykaizoku@gmail.com>
2026-03-23 09:24:45 +0100

    ajout du test de présence dans la période pour réunion

--- MyCalendar/src/test/java/fr/mycalendar/domain/event/ReunionTest.java
+++ MyCalendar/src/test/java/fr/mycalendar/domain/event/ReunionTest.java
@@ -4,6 +4,7 @@ import fr.mycalendar.domain.vo.*;
 import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,5 +23,21 @@ class ReunionTest {
                 new Participants(List.of(new Participant("Alice")))
         );
         assertNotNull(reunion);
+    }
+
+    @Test
+    void estDansPeriode() {
+        Evenement reunion = new Reunion(
+                EventId.nouveau(),
+                new TitreEvenement("Point hebdo"),
+                new DateEvenement(LocalDate.of(2023, 1, 1)),
+                new HeureDebut(LocalTime.of(14, 0)),
+                new DureeEvenement(Duration.ofMinutes(60)),
+                new DescriptionEvenement("Status"),
+                new Lieu("Salle A"),
+                new Participants(List.of(new Participant("Alice")))
+        );
+        Periode p = new Periode(LocalDateTime.of(2023, 1, 1, 13, 0), LocalDateTime.of(2023, 1, 1, 15, 0));
+        assertTrue(((Reunion) reunion).estDansPeriode(p));
     }
 }
